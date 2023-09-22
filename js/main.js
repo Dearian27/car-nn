70f17fc5c5c1d6bd92296d650e5632db03bd58b2
dearian <dearian2706@gmail.com>
2023-09-22 19:34:03 +0300

    camera chasing, borders

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 const canvas = document.getElementById('canvas');
 canvas.height = window.innerHeight;
-canvas.width = 200;
+canvas.width = 260;
 
 const ctx = canvas.getContext('2d');
 const road = new Road(canvas.width/2, canvas.width*0.9);
-const car = new Car(100, 100, 30, 50);
+const car = new Car(road.getLaneCenter(2), 500, 30, 50);
 
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
-  road.draw(ctx);
+  
+  ctx.save();
+  ctx.translate(0, -car.y+canvas.height * 0.5)
 
+  road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
--- js/road.js
+++ js/road.js
@@ -1,5 +1,5 @@
 class Road {
-  constructor(x, width, laneCount=3) {
+  constructor(x, width, laneCount=4) {
     this.x = x;
     this.width = width;
     this.laneCount = laneCount;
@@ -10,21 +10,46 @@ class Road {
     const infinity = 1000000;
     this.top = -infinity;
     this.bottom = infinity;
+
+    this.borders = [
+      [
+        {x: this.left, y: this.top},
+        {x: this.left, y: this.bottom},
+      ],
+      [
+        {x: this.right, y: this.top},
+        {x: this.right, y: this.bottom},
+      ]
+    ]
   }
+
+  getLaneCenter(laneIndex) {
+    const lineWidth = this.width / this.laneCount;
+    return this.left + lineWidth / 2 + Math.min(laneIndex, this.laneCount-1) * lineWidth;
+  }
+
   draw(ctx) {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
 
-    for(let i=0; i <= this.laneCount; i++) {
+    for(let i=1; i < this.laneCount; i++) {
       const x = lerp(
         this.left,
         this.right,
         i / this.laneCount
       )
+      ctx.setLineDash([20, 20]);
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
+    ctx.setLineDash([]);
+    this.borders.forEach(border => {
+      ctx.beginPath();
+      ctx.moveTo(border[0].x, border[0].y);
+      ctx.lineTo(border[1].x, border[1].y);
+      ctx.stroke();
+    })
   }
 }
