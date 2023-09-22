12ceff52c92c29a8ff49991eba480cde79ea51ca
dearian <dearian2706@gmail.com>
2023-09-22 16:46:06 +0300

    speed acceleraiton limit

--- js/car.js
+++ js/car.js
@@ -6,31 +6,36 @@ class Car {
     this.height = height;
     
     this.speed = 0;
-    this.maxSpeed = 4;
+    this.maxSpeed = 6;
+    this.maxBackSpeed = 4;
     this.controls = new Controls();
   }
   update() {
     if(this.controls.space) {
       if (this.speed > 0) {
-        if(this.speed >= 0.3) this.speed = (this.speed * 10 - 3) / 10
-        else this.speed = (this.speed * 10 - 1) / 10
+        if(this.speed >= 0.3) this.speed = (this.speed * 10 - 3) / 10;
+        else if(this.speed < 0.1) this.speed = 0;
+        else this.speed = (this.speed * 10 - 1) / 10;
       } else if (this.speed < 0) {
-        if(this.speed <= 0.3) this.speed = (this.speed * 10 + 3) / 10
+        if(this.speed <= 0.3) this.speed = (this.speed * 10 + 3) / 10;
+        else if(this.speed < 0.1) this.speed = 0;
         else this.speed = (this.speed * 10 + 1) / 10;
       }
       this.y += this.speed;
     }
     else if(this.controls.forward) {
       this.speed = (this.speed * 10 - 1) / 10;
+      this.speed = this.speed > this.maxSpeed ? this.maxSpeed : this.speed;
       this.y += this.speed;
     } else if(this.controls.reverse) {
       this.speed = (this.speed * 10 + 1) / 10;
+      this.speed = this.speed > this.maxBackSpeed ? this.maxBackSpeed : this.speed;
       this.y += this.speed;
     } else if (this.speed > 0) {
-      this.speed = (this.speed * 10 - 1) / 10;
+      this.speed = (this.speed * 100 - 7) / 100;
       this.y += this.speed;
     } else if (this.speed < 0) {
-      this.speed = (this.speed * 10 + 1) / 10;
+      this.speed = (this.speed * 100 + 7) / 100;
       this.y += this.speed;
     }
   }
