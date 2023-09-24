310179d02ae3982078d76794673b59151a4d0d8f
dearian <dearian2706@gmail.com>
2023-09-24 23:08:13 +0300

    ai car

--- js/car.js
+++ js/car.js
@@ -13,8 +13,12 @@ class Car {
 
     this.angle = 0;
 
+    this.useBrain = controlType == 'AI';
     if(controlType !== 'DUMMY') {
       this.sensor = new Sensor(this);
+      this.brain = new NeuralNetwork(
+        [this.sensor.rayCount, 6, 4]
+      );
     }
     this.controls = new Controls(controlType);
   }
@@ -67,8 +71,8 @@ class Car {
     
     // rotate
     if(this.controls.left) {
-      if(this.speed > 0.3) this.angle += 0.03;
-      else if(this.speed < -0.3) this.angle -= 0.03;
+      if(this.speed > 0.3) this.angle += 0.027;
+      else if(this.speed < -0.3) this.angle -= 0.027;
     } 
     if(this.controls.right) {
       if(this.speed > 0.3) this.angle -= 0.027;
@@ -81,8 +85,20 @@ class Car {
       this.polygon = this.#createPolygon();
       this.damaged = this.assessDamage(roadBorders, traffic);
     }
-    this.sensor?.update(roadBorders);
-    // this.sensor?.update(traffic);
+
+    if(this.sensor) {
+      this.sensor.update(roadBorders, traffic);
+      const offsets = this.sensor.readings.map(
+        s => s == null ? 0 : 1 - s.offset
+      )
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+      if(this.useBrain) {
+        this.controls.forward = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.reverse = outputs[3];
+      }
+    }
   }
   assessDamage(roadBorders, traffic) {
     for(let i = 0; i < roadBorders.length; i++) {
--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
-const canvas = document.getElementById('canvas');
-canvas.height = window.innerHeight;
-canvas.width = 260;
+const carCanvas = document.getElementById('carCanvas');
+carCanvas.height = window.innerHeight;
+carCanvas.width = 260;
 
-const ctx = canvas.getContext('2d');
-const road = new Road(canvas.width/2, canvas.width*0.9);
-const car = new Car(road.getLaneCenter(2), 100, 30, 50, 'KEYS', 11);
+const networkCanvas = document.getElementById('networkCanvas');
+networkCanvas.height = window.innerHeight;
+networkCanvas.width = 300;
+
+const networkCtx = carCanvas.getContext('2d');
+const carCtx = carCanvas.getContext('2d');
+
+const road = new Road(carCanvas.width/2, carCanvas.width*0.9);
+const car = new Car(road.getLaneCenter(2), 100, 30, 50, 'AI', 11);
+// const car = new Car(road.getLaneCenter(2), 100, 30, 50, 'KEYS', 11);
 
 const traffic = [
-  new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY'),
-  new Car(road.getLaneCenter(3), -400, 30, 50, 'DUMMY'),
-  new Car(road.getLaneCenter(2), -300, 30, 50, 'DUMMY'),
-  new Car(road.getLaneCenter(3), -250, 30, 50, 'DUMMY'),
-  new Car(road.getLaneCenter(0), -150, 30, 50, 'DUMMY'),
-  new Car(road.getLaneCenter(0), -400, 30, 50, 'DUMMY'),
-  new Car(road.getLaneCenter(1), -500, 30, 50, 'DUMMY'),
+  // new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY'),
+  // new Car(road.getLaneCenter(3), -400, 30, 50, 'DUMMY'),
+  new Car(road.getLaneCenter(2), 0, 30, 50, 'DUMMY'),
+  // new Car(road.getLaneCenter(3), -250, 30, 50, 'DUMMY'),
+  // new Car(road.getLaneCenter(0), -150, 30, 50, 'DUMMY'),
+  // new Car(road.getLaneCenter(0), -400, 30, 50, 'DUMMY'),
+  // new Car(road.getLaneCenter(1), -500, 30, 50, 'DUMMY'),
 ]
 
 function animate() {
@@ -22,15 +29,15 @@ function animate() {
   }
   
   car.update(road.borders, traffic);
-  canvas.height = window.innerHeight;
+  carCanvas.height = window.innerHeight;
   
-  ctx.save();
-  ctx.translate(0, -car.y+canvas.height * 0.7)
+  carCtx.save();
+  carCtx.translate(0, -car.y+carCanvas.height * 0.7)
 
-  road.draw(ctx);
-  car.draw(ctx);
+  road.draw(carCtx);
+  car.draw(carCtx);
   for(let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(ctx);
+    traffic[i].draw(carCtx);
   }
   requestAnimationFrame(animate);
 }
